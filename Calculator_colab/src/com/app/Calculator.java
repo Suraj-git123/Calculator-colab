68ad823bec3ae441f6a346eea8ce120ae0c77625
Suraj-git123 <ingolesp100@gmail.com>
2025-01-28 18:36:15 +0530

    add method added

--- Calculator_colab/src/com/app/Calculator.java
+++ Calculator_colab/src/com/app/Calculator.java
@@ -1,5 +1,15 @@
 package com.app;
 
 public class Calculator {
+	
+	public int add(int a , int b) {
+		int c = a + b;
+		return c;
+	}
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		int i = c.add(10, 20);
+		System.out.println("Addition :" + i);
+	}
 
 }
