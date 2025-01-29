b712ab45db4a539043a0701aad4e78783a59e775
krushnachandane <krushnachandane05@gmail.com>
2025-01-29 19:07:41 +0530

    add to some correction in sub method and main method

--- Calculator_colab/src/com/app/Calculator.java
+++ Calculator_colab/src/com/app/Calculator.java
@@ -6,16 +6,16 @@ public class Calculator {
 		int c = a + b;
 		return c;
 	}
-	public int sum(int a,int b){
+	public int sub(int a,int b){
 		int c=a-b;
 		return c;
 	}
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
 		int i = c.add(10, 20);
-		int x = c.sum(20,10);
+		int x = c.sub(20,10);
 		System.out.println("Addition :" + i);
-		System.out.println("Subtraction :"+ z);
+		System.out.println("Subtraction :"+x);
 		
 	}
 
