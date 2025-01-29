6f3f2bfa72939e7b07d5034e1e7f4a70032106f1
krushnachandane <krushnachandane05@gmail.com>
2025-01-29 19:43:22 +0530

    add mul and div.called those methods in main

--- Calculator_colab/src/com/app/Calculator.java
+++ Calculator_colab/src/com/app/Calculator.java
@@ -10,12 +10,29 @@ public class Calculator {
 		int c=a-b;
 		return c;
 	}
+	
+	public int mul(int a , int b)
+	{
+		int c = a*b;
+		return c;
+	}
+	
+	public int div(int p , int q)
+	{
+		int r = p/q;
+		return r;
+	}
+	
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
 		int i = c.add(10, 20);
 		int x = c.sub(20,10);
+		int	y = c.mul(2,4);
+		int z = c.div(2,11);
 		System.out.println("Addition :" + i);
 		System.out.println("Subtraction :"+x);
+		System.out.println("Multiplication :"+y);
+		System.out.println("Division :"+z);
 		
 	}
 
